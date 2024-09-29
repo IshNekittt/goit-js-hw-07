2766c1988bb977cb30777c1981998e9dc5cccbb6
Nekittt <nektoniks@gmail.com>
2024-09-29 15:33:11 +0300

    Update task 4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,20 @@
-
+const form = document.forms[0];
+form.addEventListener("submit", sendingForm);
+function sendingForm(e) {
+  e.preventDefault();
+  const elemArray = Array.from(e.currentTarget.elements);
+  const resultObj = {};
+  for (const elem of elemArray) {
+    if (elem.tagName === "INPUT") {
+      const processedValue = elem.value.trim();
+      if (!processedValue) {
+        alert("All form fields must be filled in");
+        return;
+      } else {
+        resultObj[elem.name] = processedValue;
+      }
+    }
+  }
+  console.log(resultObj);
+  e.currentTarget.reset();
+}
