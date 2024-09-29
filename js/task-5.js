1fb63142793c5cc00e8d086084e464e0a90e4bbd
Nekittt <nektoniks@gmail.com>
2024-09-29 16:53:08 +0300

    Update task 5 and update task 4's buttons

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,35 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const buttonChange = document.querySelector(".change-color");
+buttonChange.addEventListener("click", setBackgroundColor);
+
+function setBackgroundColor() {
+  const newColor = getRandomHexColor();
+  const body = document.querySelector("body");
+  const outputSpan = document.querySelector(".color");
+  body.style.backgroundColor = newColor;
+  outputSpan.textContent = newColor;
+
+  // Я додав ще функционалу для кращого відображення тексту і його читабельності.
+  // Так як змінювати оригінальний код заборонено, я реалізував це через querySelectorAll
+  const paragraphs = document.querySelectorAll("p");
+  if (isColorDark(newColor)) {
+    paragraphs[1].style.color = "#fff";
+  } else {
+    paragraphs[1].style.color = "#000";
+  }
+}
+
+function isColorDark(hexColor) {
+  hexColor = hexColor.replace("#", "");
+
+  const r = parseInt(hexColor.slice(0, 2), 16);
+  const g = parseInt(hexColor.slice(2, 4), 16);
+  const b = parseInt(hexColor.slice(4, 6), 16);
+
+  const brightness = 0.299 * r + 0.587 * g + 0.114 * b;
+
+  return brightness < 128;
+}
